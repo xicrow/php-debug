e5ed8b94ebd224b2d5c103f30633bd53a25fc061
Jan Ebsen <xicrow@gmail.com>
2016-03-22 13:56:44 +0100

    Add empty test for Profiler, for now

---
+++ tests/ProfilerTest.php
@@ -0,0 +1,26 @@
+<?php
+use Xicrow\Debug\Profiler;
+
+/**
+ * Class ProfilerTest
+ *
+ * Missing tests:
+ *
+ * @covers \Xicrow\Debug\Profiler::init
+ * @covers \Xicrow\Debug\Profiler::add
+ * @covers \Xicrow\Debug\Profiler::start
+ * @covers \Xicrow\Debug\Profiler::stop
+ * @covers \Xicrow\Debug\Profiler::custom
+ * @covers \Xicrow\Debug\Profiler::callback
+ * @covers \Xicrow\Debug\Profiler::getStats
+ * @covers \Xicrow\Debug\Profiler::getLastItemName
+ * @covers \Xicrow\Debug\Profiler::formatDateTime
+ * @covers \Xicrow\Debug\Profiler::formatForUnits
+ * @covers \Xicrow\Debug\Profiler::formatMiliseconds
+ * @covers \Xicrow\Debug\Profiler::formatBytes
+ */
+class ProfilerTest extends PHPUnit_Framework_TestCase {
+	public function test() {
+		// Dummy test to avoid PHPunit complaining...
+	}
+}
